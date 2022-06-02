134928955eea3e723acab8dbcc9b001e0a5dd47d
Marva82 <aaronuram123@gmail.com>
2022-06-02 00:24:18 -0500

    adding Sum_II

---
+++ Sum_II.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        
+        int lo = 0, hi = numbers.length - 1;
+        
+        while(lo < hi) {
+            int curSum = numbers[lo] + numbers[hi];
+            if(curSum == target) {
+                return new int[]{lo + 1, hi + 1};
+            } else if (curSum > target) {
+                hi -= 1;
+            }else {
+                lo++;
+            }
+        }
+        return new int[]{-1, -1};
+        
+    }
+}
