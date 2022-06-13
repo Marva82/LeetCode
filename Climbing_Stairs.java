c8380abc68b0a4b6e0b4f578d4be45e6d0751c8a
Marva82 <aaronuram123@gmail.com>
2022-06-13 06:14:27 -0500

    climbing stairs

---
+++ Climbing_Stairs.java
@@ -0,0 +1,20 @@
+//Climbing Stairs Problem - Dynamic Programming
+class Solution {
+    public int climbStairs(int n) {
+        
+        if(n == 1) {
+            return 1;
+        }
+        
+        int first = 1;
+        int second = 2;
+        
+        for(int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+        
+    }
+}
