308973c83aec7d3df81a422ad1f7b946deb1ba2e
Marva82 <aaronuram123@gmail.com>
2022-07-16 19:56:07 -0500

    Top K frequent elements

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -7,16 +7,22 @@ class Solution:
         #Array used to store frequency of input elements
         freq = [[] for i in range(len(nums) + 1)]
         
+        #Storing the values in count
         for n in nums:
             count[n] = 1 + count.get(n,0)
         
+        #Returning the key, value pair that was added to dictionary
+        #append key value of n, c number of times
         for n, c in count.items():
             freq[c].append(n)
             
         res = []
         
+        #Iterate thru array in descending order
         for i in range(len(freq) - 1, 0, -1):
+            #Go thru every sublist, could contain multiple values
             for n in freq[i]:
                 res.append(n)
+                #Gauranteed to have k number of values
                 if len(res) == k:
                     return res
