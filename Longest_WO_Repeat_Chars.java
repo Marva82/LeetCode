1eed3a16c14ac07abc93af18de41743dc352e541
Marva82 <aaronuram123@gmail.com>
2022-07-19 00:59:26 -0500

    Longest Char set update

--- Longest_WO_Repeat_Chars.java
+++ Longest_WO_Repeat_Chars.java
@@ -10,10 +10,15 @@ class Solution {
         
         for(right = 0, left = 0; right < n; right++) {
             if(map.containsKey(s.charAt(right))) {
+                //Removes the left most character since it is a duplicate
+                //Increment the left pointer for sliding window
                 left = Math.max(map.get(s.charAt(right)), left);
             }
-            res = Math.max(res, right - left + 1);
+            
+            //Add right most character to the set
             map.put(s.charAt(right), right + 1);
+            //Update the results variable if the current window size is greater than previous window
+            res = Math.max(res, right - left + 1);
         }
         return res;
         
